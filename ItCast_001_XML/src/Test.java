dd7082e6c1ac26d7e957590bb54778e1d44f5b39
zhaoccx <zhaoccx@hotmail.com>
2015-04-10 23:51:29 +0800

    Init XML

---
+++ ItCast_001_XML/src/Test.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test {
+
+	@SuppressWarnings("unused")
+	public static void main(String[] args) {
+
+		@SuppressWarnings("rawtypes")
+		List list = new ArrayList();
+		// Map map = new HashMap();
+
+		// ctrl + 1
+		// alt + / 内容提示
+		// ctrl + shift + o 包整理
+
+		String str = "<table>" + "<tr>" + "<td>" + "</td>" + "</tr>" + "</table>";
+
+		if (true) {
+
+		}
+
+		if (true) {
+
+		}
+
+	}
+}
